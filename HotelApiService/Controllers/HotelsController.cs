2d15510400390d37dc066382021fc8aec298fcf0
Amar78612 <60280308+Amar78612@users.noreply.github.com>
2022-05-20 20:11:47 +0530

    Update HotelsController.cs

--- HotelApiService/Controllers/HotelsController.cs
+++ HotelApiService/Controllers/HotelsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HotelApiService.Models;
+using HotelApiService.Repository;
 
 namespace HotelApiService.Controllers
 {
